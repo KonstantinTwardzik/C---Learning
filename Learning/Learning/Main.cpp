b5bfd0d576adfb40d2d1388339108b950a080773
Konstantin <twardzk@hochschule-trier.de>
2018-10-08 15:24:07 +0200

    First cpp file

---
+++ Learning/Learning/Main.cpp
@@ -0,0 +1,15 @@
+#include "iostream"
+using namespace std;
+
+int main()
+{
+	int a, b, sum;
+	cout << "Enter two values, u want to sum up: \n a: ";
+	cin >> a;
+	cout << "\n b: ";
+	cin >> b;
+	sum = a + b;
+	cout << "\n The sum of those values is: " << sum;
+	cin >> a;
+	return 0;
+}
